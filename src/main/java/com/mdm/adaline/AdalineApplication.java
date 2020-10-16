a4d42c544b7bb3c267801bf8a6f098a757c43ee7
Gerardo Daniel Tolentino <gerardzt94@gmail.com>
2020-10-16 17:07:04 -0500

    001First

---
+++ src/main/java/com/mdm/adaline/AdalineApplication.java
@@ -0,0 +1,13 @@
+package com.mdm.adaline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdalineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdalineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdm/adaline/AdalineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdm.adaline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdalineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
